c7a2ff31275250c69400302fc9b23272f609c190
Jason Swingen <jason.swingen@gmail.com>
2016-05-09 18:36:31 -0500

    added smooth scolling to sections script

--- js/_main.js
+++ js/_main.js
@@ -10,6 +10,22 @@ $(document).ready(function(){
   });
 });
 
+// Smooth scrolling to sections
+$(function() {
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') === this.pathname.replace(/^\//,'') && location.hostname === this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top // Amount of offset
+        }, 600);
+        return false;
+      }
+    }
+  });
+});
+
 // FitVids
 $(document).ready(function(){
 	// Target your .container, .wrapper, .post, etc.
@@ -23,20 +39,4 @@ $("#markdown-toc").prepend("<li><h6>{{ site.data.messages.locales[site.locale].o
 $('.totop').click(function(event){
   event.preventDefault();
   $('html, body').animate({scrollTop: 0}, 900);
-});
-
-// Smooth scrolling to sections
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html,body').animate({
-          scrollTop: target.offset().top
-        }, 900);
-        return false;
-      }
-    }
-  });
 });
